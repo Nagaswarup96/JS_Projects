a1ed80e2bb6e96f9b21b89ec5e5d644c1949c58b
Nagaswarup D P <85451595+Nagaswarup96@users.noreply.github.com>
2021-08-11 00:57:59 +0530

    First Version

---
+++ WebBlog/login.js
@@ -0,0 +1,51 @@
+const users = [
+    {   
+        email:"nagaswarupdp@gmail.com",
+        password:"N@g@6026",
+        username: "nagaswarup"
+    },
+    {
+        email:"naga123@gmail.com",
+        password:"1234567",
+        username:"Swarup"
+    }
+];
+
+const inpE = document.querySelector(".login");
+const inpP = document.querySelector(".pass");
+const btn = document.querySelector(".loginB");
+const frm = document.querySelector('.formS');
+const box = document.querySelector(".box");
+const msg =document.querySelector(".error");
+const cont = document.querySelector(".contact");
+
+console.log(inpE.value);
+
+let usern = "";
+let flag = 0;
+
+function validation(){
+    
+    users.forEach((user)=>{
+        if((inpE.value === user.email) && (inpP.value === user.password)){
+            frm.setAttribute("action", "index.html");
+            usern = user.username;
+            flag = 1;
+            localStorage.setItem("flags", flag);
+            localStorage.setItem("usernam", usern);
+        }
+        else if ((inpE.value != user.email) || (inpP.value != user.password)){
+           box.setAttribute("style", "display : block");
+           msg.setAttribute("style", "color:red");
+           btn.setAttribute("style", "margin-top:0")
+
+            }
+        
+    })
+   
+}
+
+btn.addEventListener('click', validation);
+cont.addEventListener('click',()=>{
+    cont.setAttribute("href", "#contact");
+});
---
+++ WebBlog/single.js
@@ -0,0 +1,135 @@
+$('.menu-toggle').on('click', function () {
+    $('.nav').toggleClass('showing');
+    $('.page-wrapper').toggleClass('move');
+});
+
+
+const news =
+    [
+        {
+            id: 1,
+            headline: "Multiple Raids In J&K In Massive Crackdown In Terror Funding Case",
+            news: `New Delhi: The National Investigation Agency (NIA) is conducting raids 
+      at 45 locations across Jammu 
+      and Kashmir's Anantnag district in connection with a terror funding case.
+      The raids, assisted by J&K Police and CRPF, are being conducted across Doda, Kishtwar, Ramban, Anantnag, Budgam, Rajouri, Doda, and Shopian, among others.
+        The home of a member of Jamaat-e-Islami (JeI), Gul Mohammad War is also raided.
+        Last month, in a Home Ministry review there was a discussion about the growing influence of Jamaat-e-Islami, an outfit that was banned in 2019.
+        Home Minister Amit Shah had asked security forces to take action in order to stop radicalisation of youth.
+        On July 10, the investigating agency had arrested six people from across Jammu and Kashmir in connection with a terror funding case.
+        The raids came a day after eleven employees of the J&K government were sacked for having terror links. Among those dismissed were 
+        two sons of terror outfit Hizbul Mujahideen founder Syed Salahudin.
+        A Delhi Court had earlier ordered the framing of charges against 
+        four alleged Hizb-ul-Mujahideen terrorists observing prima facie
+         evidence against them for receiving funds from Pakistan to plot
+          terrorist activities in Jammu and Kashmir.`
+        },
+        {
+            id: 2,
+            headline: "Fierce Fighting Rages In Centre Of Major Afghan City Kunduz: Official",
+            news: `Kunduz, Afghanistan: Afghan forces and the Taliban were fighting fiercely Sunday in the centre of Kunduz, officials and residents said, after the insurgents captured two other provincial capitals in the last 48 hours.
+        The fall of the northern city would be a major blow for the central government, which has largely abandoned fighting in the countryside to defend urban centres against Taliban attacks.
+        
+        "Fierce street-to-street fighting is ongoing in different parts of the city," Amruddin Wali, a member of the Kunduz provincial council, told AFP.
+        
+        "Some security forces have retreated towards the airport.
+        
+        The Taliban have taken two provincial capitals since Friday, but Kunduz would be the most significant to fall since the insurgents launched an offensive in May as foreign forces began the final stages of their withdrawal.
+        
+        On Friday the Taliban seized their first provincial capital, Zaranj in Nimroz, and followed it up a day later by taking Sheberghan in Jawzjan.
+        
+        "The Taliban have reached the main square of the city. Aircraft are bombing them," said Abdul Aziz, a Kunduz resident reached by phone.
+        
+        "There is total chaos."
+        
+        Fighting was also reported on the outskirts of Herat, in the west, and Lashkar Gah and Kandahar in the south.
+        
+        The pace of Taliban advances has caught government forces flatfooted, but they had some respite late Saturday after US warplanes bombed Taliban positions in Sheberghan, the Jawzjan province capital seized earlier in the day.`
+        },
+        {
+            id: 3,
+            headline: "Sale of Cairn shares by govt a bad decision?",
+            news: `On Thursday, the government introduced a Bill in Parliament to scrap the tax rule that gave the
+         tax department power to go 50 years back and slap capital gains levies wherever ownership had changed 
+         hands overseas but business assets were in India.\n
+         Faced with prospect of its assets across the globe being seized just like Pakistan and Venezuela,
+          the government decided to scrap retrospective taxation but the international embarrassment could have 
+          been avoided had 'attached' shares of Britain's Cairn Energy Plc not been sold, according to tax and legal experts.
+            On Thursday, the government introduced a Bill in Parliament to scrap the tax rule that gave the tax department power to go
+        50 years back and slap capital gains levies wherever ownership had changed hands overseas but business assets were in India.
+
+        The 2012 legislation was used to levy a cumulative of Rs 1.10 lakh crore of tax on 17 entities, including UK telecom giant
+         Vodafone, but substantial punitive action was taken only in the case of Cairn.
+
+        The income tax department not just sold Cairn's near 10 per cent shareholding in its erstwhile Indian subsidiary but also seized its dividends totalling Rs 1,140 crore and stopped tax refunds of Rs 1,590 crore.
+
+            "The shares the firm held in Cairn India (renamed Vedanta Ltd) were attached by the income tax department soon after it served an initial assessment of Rs 10,247 crore tax demand on Cairn Energy in January 2014.`
+        },
+        {
+            id: 4,
+            headline: "Opposition's Message To Centre Ahead Of Final Week Of Monsoon Session",
+            news: `New Delhi: With just a week to go before parliament's Monsoon Session ends, 
+            Trinamool Congress's Derek O'Brien demanded today that Prime Minister Narendra Modi 
+            attends the proceedings and listen to the demands of the opposition, which has been 
+            clamouring for a discussion on the Pegasus spyware row, the farmers' issues and several 
+            others. Mr O'Brien tweeted a three-minute video of various opposition parties pushing their in parliament,
+             captioned "Mr Modi come listen to us".`
+        },
+        {
+            id: 5,
+            headline: "In A First in K'taka, BS Yediyurappa Gets Cabinet Rank Status Courtesy Bommai, But He Declines",
+            news: `The Karnataka government on Saturday issued an order providing former Chief Minister B S Yediyurappa all facilities on par with Cabinet-rank ministers. This will be applicable until Chief Minister Basavaraj Bommai is in office, an official notification from the protocol wing of the Department of Personnel and Administrative Reforms (DPAR) said.
+
+            Yediyurappa, however, on Sunday urged CM Bommai to withdraw his order to give him cabinet rank status. “I request you to provide me only those facilities which are given to an immediate former chief minister and withdraw the order giving me a cabinet rank,” Yediyurappa said in his letter to Bommai, which was made available to the media.
+            
+            Ending months of speculation over his exit, Yediyurappa on July 26 had stepped down as the Chief Minister of Karnataka, coinciding with his government completing two years in office. Bommai, who succeeded Yediyurapp,a is expected to remain in the post until the next assembly elections in 2023.
+            
+            Yediyurappa does not hold any official position other than being a MLA from Shikaripura assembly constituency. According to official sources, other than the salary, the cabinet rank minister is entitled to certain allowances and facilities for vehicle, official accommodation, among others.
+            
+            `
+        },
+        {
+            id: 6,
+            headline: "Taliban Captures 2 More Cities, Afghan Commandos Launch Counter-Attacks",
+            news: `Kabul, Afghanistan: The Taliban captured two more provincial capitals Sunday as they gained ground in their fight to take over Afghanistan's cities after seizing much of the countryside in recent months.
+            The insurgents have snatched up four provincial capitals since Friday in a rapid offensive that appears to have overwhelmed government forces.
+            
+            Kunduz and Sar-e-Pul in the north fell within hours of each other Sunday, lawmakers and residents in the cities confirmed, but not without fierce fighting.
+            
+            A Kunduz resident described the city as being enveloped in "total chaos".
+            
+            "After some fierce fighting, the mujahideen, with the grace of God, captured the capital of Kunduz," the Taliban said in a statement.
+            
+            "The mujahideen also captured Sar-e-Pul city, the government buildings and all the installations there."
+            
+            Parwina Azimi, a women's rights activist in Sar-e-Pul, told AFP by phone that government officials and the remaining forces had retreated to a barracks about three kilometres (two miles) from the city.
+            
+            "A plane came... but could not (land)," she said.Kunduz, however, is the most significant Taliban gain since the insurgents launched an offensive in May as foreign forces began the final stages of their withdrawal.
+
+            It has been a perennial target for the Taliban, who briefly overran the city in 2015 and again in 2016 but never managed to hold it for long.
+            
+            The ministry of defence said government forces were fighting to retake key installations.
+            
+            "The commando forces have launched a clearing operation. Some areas, including the national radio and TV buildings, have been cleared of the terrorist Taliban," it said in a statement.
+            
+            Kabul's inability to hold the north may prove crucial to the government's long-term survival.
+            
+            The region continues to be home to several militias and is also a fertile recruiting ground for the country's armed forces.`
+        }
+
+
+    ]
+
+const headlines = document.querySelector(".headline");
+const data = document.querySelector('.infor');
+
+let x = localStorage.getItem("valuex");
+for (let i = 0; i <= news.length; i++) {
+
+    if (parseInt(x) === news[i].id) {
+        headlines.innerHTML = news[i].headline;
+        data.innerHTML = news[i].news;
+    }
+
+}
+console.log(x);
